1b1861a6dfedff9b5067fbcc3576991808d16aec
JULIANO ROQUE VIEIRA <julianovieira@yahoo.com.br>
2023-07-14 19:36:14 -0300

    Arquivo de revisão criado

---
+++ revisao.js
@@ -0,0 +1,23 @@
+function imprimir(texto) {
+    console.log(texto)
+}
+
+function calcularArea(base, altura) {
+    return Number(base * altura).toFixed(2)
+}
+
+imprimir('Ola mundo');
+let area = calcularArea(10, 20);
+imprimir(area);
+
+function calcularFactorial(numero) {
+    if (numero === 0) {
+        return 1;
+    } else {
+        return numero * calcularFactorial(numero - 1);
+    }
+}
+
+let numero = 5
+let fat = calcularFactorial(numero)
+imprimir("O fatorial de "+numero+" e "+fat)
